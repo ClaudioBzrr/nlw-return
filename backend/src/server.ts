7f16a651d26ed2448fa7320fb4b43c5800d29b95
claudiobzrr <claudio.bezerra1998@gmail.com>
2022-06-28 21:34:11 -0300

    100% de coverage on testing

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,11 @@
 import express from 'express'
 import 'dotenv/config'
+import cors from 'cors'
 import { routes } from './routes'
 
 
 const app = express()
+app.use(cors())
 const port = process.env.SERVER_PORT
 
 app.use(express.json())
